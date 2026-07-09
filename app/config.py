21efb381123bf4c77230d6f0c403be288af30aee
maxsantosmotta-web <maxsantosmotta@gmail.com>
2026-07-09 16:03:29 -0300

    feat(database): adicionar DATABASE_URL nas configs

--- app/config.py
+++ app/config.py
@@ -11,6 +11,7 @@ class Settings:
     app_name: str = os.getenv("APP_NAME", "DomnAI")
     app_version: str = os.getenv("APP_VERSION", "0.1.0")
     environment: str = os.getenv("ENVIRONMENT", "development")
+    database_url: str | None = os.getenv("DATABASE_URL")
 
 
 settings = Settings()
