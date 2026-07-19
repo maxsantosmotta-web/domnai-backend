e0e26522e8d4caddda91745f30d3df50d2e4dac3
maxsantosmotta-web <maxsantosmotta@gmail.com>
2026-07-18 21:31:52 -0300

    Expose new core capability contracts

--- app/domnai_core/__init__.py
+++ app/domnai_core/__init__.py
@@ -10,12 +10,32 @@ from app.domnai_core.contracts import (
     HistoryMessage,
 )
 from app.domnai_core.engine import ConversationEngine, ModelProvider
+from app.domnai_core.memory import InMemoryMemoryStore, MemoryStore, NullMemoryStore
+from app.domnai_core.persistence import (
+    ConversationRecord,
+    ConversationRepository,
+    InMemoryConversationRepository,
+    NullConversationRepository,
+)
+from app.domnai_core.providers import OpenAIResponsesProvider
+from app.domnai_core.tools import ToolCall, ToolRegistry, ToolResult
 
 __all__ = [
     "Attachment",
     "ConversationEngine",
+    "ConversationRecord",
+    "ConversationRepository",
     "ConversationRequest",
     "ConversationResponse",
     "HistoryMessage",
+    "InMemoryConversationRepository",
+    "InMemoryMemoryStore",
+    "MemoryStore",
     "ModelProvider",
+    "NullConversationRepository",
+    "NullMemoryStore",
+    "OpenAIResponsesProvider",
+    "ToolCall",
+    "ToolRegistry",
+    "ToolResult",
 ]
